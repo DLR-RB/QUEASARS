d7b132a3f6450c88852d239967bb4d59fad6f760
Daniel Leidreiter <85784783+dleidreiter@users.noreply.github.com>
2023-11-07 16:41:28 +0100

    add initial unit test to test whether queasars was installed successfully in the CI

---
+++ test/test_ci.py
@@ -0,0 +1,8 @@
+# Quantum Evolving Ansatz Variational Solver (QUEASARS)
+# Copyright 2023 DLR - Deutsches Zentrum für Luft- und Raumfahrt e.V.
+
+import queasars
+
+
+def test_ci_project_setup():
+    assert "queasars" in globals()
