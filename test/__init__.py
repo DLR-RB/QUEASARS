802df35e4238b33c5f97e29f89e636e2a1e82cac
leid_dn <daniel.leidreiter@dlr.de>
2023-11-07 09:35:33 +0100

    Fix wrong .py file header.

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-# This file is part of Quantum Reinforced Groundstation Scheduling (QUARGS).
-#
-# Copyright (c) 2023 by Deutsches Zentrum für Luft- und Raumfahrt e.V.
+# Quantum Evolving Ansatz Variational Solver (QUEASARS)
+# Copyright 2023 DLR - Deutsches Zentrum für Luft- und Raumfahrt e.V.
