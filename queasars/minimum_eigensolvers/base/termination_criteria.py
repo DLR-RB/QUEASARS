22ecf8cd5b484f2aefca7277a6e98e3d57f226ee
Daniel Leidreiter <85784783+dleidreiter@users.noreply.github.com>
2024-01-09 11:56:02 +0100

    implement a relative and absolute termination criterion based on the change in the expectation value of the best individual of a generation, prepend hadamard gates to each individual to provide a better starting point for the optimization

--- queasars/minimum_eigensolvers/base/termination_criteria.py
+++ queasars/minimum_eigensolvers/base/termination_criteria.py
@@ -32,3 +32,76 @@ class EvolvingAnsatzMinimumEigensolverBaseTerminationCriterion(ABC):
         :return: True if the algorithm should terminate, False otherwise
         :rtype: bool
         """
+
+
+class BestIndividualAbsoluteImprovementTolerance(EvolvingAnsatzMinimumEigensolverBaseTerminationCriterion):
+    """Termination criterion which terminates when the absolute change in expectation value between the best individual
+    of the current and the last generation falls below an absolute threshold value
+
+    :param minimum_absolute_improvement: expectation value improvement below which the algorithm shall terminate.
+        Must be bigger than 0
+    :type minimum_absolute_improvement: float
+    """
+
+    def __init__(self, minimum_absolute_improvement: float):
+        if minimum_absolute_improvement <= 0:
+            raise ValueError("The minimum absolute improvement parameter must be bigger than 0!")
+
+        self.minimum_absolute_improvement: float = minimum_absolute_improvement
+        self.previous_best_expectation_value: float = float("inf")
+
+    def reset_state(self) -> None:
+        self.previous_best_expectation_value = float("inf")
+
+    def check_termination(
+        self,
+        population_evaluation: BasePopulationEvaluationResult,
+        best_individual: BaseIndividual,
+        best_expectation_value: float,
+    ) -> bool:
+        if (
+            population_evaluation.best_expectation_value
+            < self.previous_best_expectation_value - self.minimum_absolute_improvement
+        ):
+            self.previous_best_expectation_value = population_evaluation.best_expectation_value
+            return False
+
+        return True
+
+
+class BestIndividualRelativeImprovementTolerance(EvolvingAnsatzMinimumEigensolverBaseTerminationCriterion):
+    """Termination criterion which terminates when the change in expectation value between
+    the best individual of the current and last generation falls below a threshold. This threshold is taken
+    as a percentage of the expectation value of the best individual of the last generation
+
+    :param minimum_relative_improvement: relative improvement in expectation value below which the algorithm shall
+        terminate. Must be in the range )0,1)
+    :type minimum_relative_improvement: float
+    """
+
+    def __init__(self, minimum_relative_improvement: float):
+        if minimum_relative_improvement <= 0 or minimum_relative_improvement > 1:
+            raise ValueError("The minimum relative improvement parameter must not exceed the range )0,1)!")
+
+        self.minimum_relative_improvement = minimum_relative_improvement
+        self.previous_best_expectation_value: float = float("inf")
+
+    def reset_state(self) -> None:
+        self.previous_best_expectation_value = float("inf")
+
+    def check_termination(
+        self,
+        population_evaluation: BasePopulationEvaluationResult,
+        best_individual: BaseIndividual,
+        best_expectation_value: float,
+    ) -> bool:
+        if self.previous_best_expectation_value == float("inf"):
+            self.previous_best_expectation_value = population_evaluation.best_expectation_value
+            return False
+        if population_evaluation.best_expectation_value < self.previous_best_expectation_value - abs(
+            self.minimum_relative_improvement * self.previous_best_expectation_value
+        ):
+            self.previous_best_expectation_value = population_evaluation.best_expectation_value
+            return False
+
+        return True
