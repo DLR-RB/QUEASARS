56375fd94f4a17c6c38c951aaac4547566fe6dbc
Daniel Leidreiter <85784783+dleidreiter@users.noreply.github.com>
2024-01-09 14:56:27 +0100

    add unit tests to check the functionality of the EVQEIndividual class

--- test/minimum_eigensolvers/evqe/test_evqe_individual.py
+++ test/minimum_eigensolvers/evqe/test_evqe_individual.py
@@ -3,6 +3,7 @@
 
 from random import Random
 
+import pytest
 from pytest import raises
 from qiskit.circuit import QuantumCircuit
 
@@ -13,6 +14,8 @@ from queasars.minimum_eigensolvers.evqe.evolutionary_algorithm.individual import
     IdentityGate,
     EVQECircuitLayer,
     EVQECircuitLayerException,
+    EVQEIndividual,
+    EVQEIndividualException,
 )
 from queasars.utility.random import new_random_seed
 
@@ -84,6 +87,13 @@ class TestEVQECircuitLayer:
                 if not isinstance(gate, IdentityGate):
                     assert gate != previous_layer.gates[i], f"invalid gate added in next_layer {i}"
 
+    def test_random_layer_seed(self):
+        layer_0 = EVQECircuitLayer.random_layer(n_qubits=12, previous_layer=None, random_seed=0)
+        assert layer_0 == EVQECircuitLayer.random_layer(n_qubits=12, previous_layer=None, random_seed=0)
+
+        layer_1 = EVQECircuitLayer.random_layer(n_qubits=12, previous_layer=layer_0, random_seed=0)
+        assert layer_1 == EVQECircuitLayer.random_layer(n_qubits=12, previous_layer=layer_0, random_seed=0)
+
     def test_parameterized_layer_gate_amount_of_parameters(self):
         circuit_layer = EVQECircuitLayer.random_layer(n_qubits=10, previous_layer=None, random_seed=0)
         parameterized_gate = circuit_layer.get_parameterized_layer_gate(layer_id=0)
@@ -137,3 +147,151 @@ class TestEVQECircuitLayer:
                 filtered_gates.remove(gate)
 
             assert len(filtered_gates) == 0, f"failed for circuit layer {i}"
+
+
+class TestEVQEIndividual:
+    def test_random_individuals_are_valid(self):
+        random_generator = Random(0)
+
+        for _ in range(0, 100):
+            individual = EVQEIndividual.random_individual(
+                n_qubits=15,
+                n_layers=10,
+                randomize_parameter_values=False,
+                random_seed=new_random_seed(random_generator),
+            )
+            assert individual.is_valid()
+
+    def test_random_individual_seed(self):
+        individual_1 = EVQEIndividual.random_individual(
+            n_qubits=15,
+            n_layers=10,
+            randomize_parameter_values=False,
+            random_seed=0,
+        )
+        assert individual_1 == EVQEIndividual.random_individual(
+            n_qubits=15,
+            n_layers=10,
+            randomize_parameter_values=False,
+            random_seed=0,
+        )
+
+        individual_2 = EVQEIndividual.random_individual(
+            n_qubits=20,
+            n_layers=8,
+            randomize_parameter_values=True,
+            random_seed=0,
+        )
+        assert individual_2 == EVQEIndividual.random_individual(
+            n_qubits=20,
+            n_layers=8,
+            randomize_parameter_values=True,
+            random_seed=0,
+        )
+
+    @pytest.fixture
+    def individual(self) -> EVQEIndividual:
+        return EVQEIndividual.random_individual(
+            n_qubits=20, n_layers=10, randomize_parameter_values=True, random_seed=0
+        )
+
+    def test_change_parameter_values(self, individual):
+        n_parameters = len(individual.get_parameter_values())
+        new_parameters = (0,) * n_parameters
+
+        new_individual = EVQEIndividual.change_parameter_values(individual=individual, parameter_values=new_parameters)
+
+        assert new_individual.n_qubits == individual.n_qubits
+        assert new_individual.layers == individual.layers
+        assert new_individual.parameter_values == new_parameters
+
+    def test_change_layer_parameter_values(self, individual):
+        layer_to_change = 4
+        n_params_before = sum(layer.n_parameters for layer in individual.layers[0:layer_to_change])
+        layer_n_params = individual.layers[layer_to_change].n_parameters
+
+        new_parameters = (0,) * layer_n_params
+        new_individual = EVQEIndividual.change_layer_parameter_values(
+            individual=individual, layer_id=layer_to_change, parameter_values=new_parameters
+        )
+
+        assert new_individual.n_qubits == individual.n_qubits
+        assert new_individual.layers == individual.layers
+        assert (
+            new_individual.get_parameter_values()[0:n_params_before]
+            == individual.get_parameter_values()[0:n_params_before]
+        )
+        assert (
+            new_individual.get_parameter_values()[n_params_before : n_params_before + layer_n_params] == new_parameters
+        )
+        assert (
+            new_individual.get_parameter_values()[n_params_before + layer_n_params :]
+            == individual.get_parameter_values()[n_params_before + layer_n_params :]
+        )
+
+    def test_add_random_layers(self, individual):
+        with raises(EVQEIndividualException):
+            EVQEIndividual.add_random_layers(
+                individual=individual, n_layers=0, randomize_parameter_values=False, random_seed=0
+            )
+
+        n_params_before = len(individual.get_parameter_values())
+        n_new_random_layers = 3
+        new_individual = EVQEIndividual.add_random_layers(
+            individual=individual, n_layers=n_new_random_layers, randomize_parameter_values=False, random_seed=0
+        )
+
+        assert new_individual.is_valid()
+        assert new_individual.n_qubits == individual.n_qubits
+        assert len(new_individual.layers) == len(individual.layers) + n_new_random_layers
+        assert new_individual.layers[: len(individual.layers)] == individual.layers
+        assert new_individual.get_parameter_values()[0:n_params_before] == individual.get_parameter_values()
+
+    def test_remove_layers(self, individual):
+        with raises(EVQEIndividualException):
+            EVQEIndividual.remove_layers(individual=individual, n_layers=0)
+        with raises(EVQEIndividualException):
+            EVQEIndividual.remove_layers(individual=individual, n_layers=len(individual.layers))
+
+        n_layers_to_remove = 4
+        n_params_upto_layer_4 = sum(layer.n_parameters for layer in individual.layers[0:-n_layers_to_remove])
+
+        new_individual = EVQEIndividual.remove_layers(individual=individual, n_layers=n_layers_to_remove)
+        assert new_individual.n_qubits == individual.n_qubits
+        assert new_individual.layers == individual.layers[0:-n_layers_to_remove]
+        assert new_individual.get_parameter_values() == individual.get_parameter_values()[0:n_params_upto_layer_4]
+
+    def test_get_genetic_distance(self, individual):
+        new_individual = EVQEIndividual.add_random_layers(
+            individual=individual, n_layers=1, randomize_parameter_values=False, random_seed=0
+        )
+        assert EVQEIndividual.get_genetic_distance(individual_1=individual, individual_2=new_individual) == 1
+
+        new_individual = EVQEIndividual.remove_layers(individual=individual, n_layers=2)
+        assert EVQEIndividual.get_genetic_distance(individual_1=individual, individual_2=new_individual) == 1
+
+    def test_get_quantum_cirucit(self, individual):
+        circuit = individual.get_quantum_circuit()
+        assert circuit.depth() == len(individual.layers) + 1
+
+    def test_get_parameterized_quantum_circuit(self, individual):
+        circuit = individual.get_parameterized_quantum_circuit()
+        assert circuit.depth() == len(individual.layers) + 1
+        assert len(circuit.parameters) == len(individual.get_parameter_values())
+
+    def test_get_partially_parameterized_quantum_circuit(self, individual):
+        layer_ids = {2, 5}
+        circuit = individual.get_partially_parameterized_quantum_circuit(layer_ids)
+        assert circuit.depth() == len(individual.layers) + 1
+        assert len(circuit.parameters) == individual.layers[2].n_parameters + individual.layers[5].n_parameters
+
+    def test_get_parameters(self, individual):
+        n_parameters = sum(layer.n_parameters for layer in individual.layers)
+        assert len(individual.get_parameter_values()) == n_parameters
+
+    def test_get_layer_parameters(self, individual):
+        assert len(individual.get_layer_parameter_values(layer_id=7)) == individual.layers[7].n_parameters
+
+    def test_get_n_controlled_gates(self, individual):
+        circuit = individual.get_quantum_circuit().decompose()
+        assert individual.get_n_controlled_gates() == circuit.count_ops()["cu3"]
