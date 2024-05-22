98531339f95fb21328997bd481dde49dab26f1ef
Daniel Leidreiter <85784783+dleidreiter@users.noreply.github.com>
2024-05-22 12:41:12 +0200

    add some more fields to the benchmark result serialization, allow all fields to be None

--- experiment_scripts/benchmark_result_serialization.py
+++ experiment_scripts/benchmark_result_serialization.py
@@ -3,7 +3,7 @@
 
 from dataclasses import dataclass
 from json import JSONEncoder, JSONDecoder
-from typing import Any
+from typing import Any, Optional
 
 from qiskit.result import QuasiDistribution
 
@@ -20,32 +20,36 @@ from queasars.minimum_eigensolvers.base.serialization import (
 
 @dataclass
 class VQABenchmarkResult:
-    n_qubits: int
-    instance_nr: int
-    seed: int
-    best_expectation_value: float
-    best_parameter_values: list[float]
-    expectation_evaluation_counts: list[float]
-    expectation_values: list[float]
-    measurement_distribution: QuasiDistribution
-    state_translations: dict[int, JobShopSchedulingResult]
-    optimal_makespan: int
-    min_energy: float
-    max_opt_energy: float
-    max_energy: float
+    n_qubits: Optional[int] = None
+    instance_nr: Optional[int] = None
+    seed: Optional[int] = None
+    best_expectation_value: Optional[float] = None
+    best_parameter_values: Optional[list[float]] = None
+    expectation_evaluation_counts: Optional[list[float]] = None
+    expectation_values: Optional[list[float]] = None
+    measurement_distribution: Optional[QuasiDistribution] = None
+    state_translations: Optional[dict[int, JobShopSchedulingResult]] = None
+    optimal_makespan: Optional[int] = None
+    min_penalty: Optional[float] = None
+    min_energy: Optional[float] = None
+    min_subopt_energy: Optional[float] = None
+    max_opt_energy: Optional[float] = None
+    max_energy: Optional[float] = None
 
 
 @dataclass
 class EVQEBenchmarkResult:
-    n_qubits: int
-    instance_nr: int
-    seed: int
-    result: EvolvingAnsatzMinimumEigensolverResult
-    state_translations: dict[int, JobShopSchedulingResult]
-    optimal_makespan: int
-    min_energy: float
-    max_opt_energy: float
-    max_energy: float
+    n_qubits: Optional[int] = None
+    instance_nr: Optional[int] = None
+    seed: Optional[int] = None
+    result: Optional[EvolvingAnsatzMinimumEigensolverResult] = None
+    state_translations: Optional[dict[int, JobShopSchedulingResult]] = None
+    optimal_makespan: Optional[int] = None
+    min_penalty: Optional[float] = None
+    min_energy: Optional[float] = None
+    min_subopt_energy: Optional[float] = None
+    max_opt_energy: Optional[float] = None
+    max_energy: Optional[float] = None
 
 
 class ResultEncoder(JSONEncoder):
@@ -64,6 +68,12 @@ class ResultEncoder(JSONEncoder):
             return self._jssp_encoder.default(o)
 
         if isinstance(o, VQABenchmarkResult):
+
+            if o.state_translations is None:
+                state_translations = None
+            else:
+                state_translations = [[key, self.default(value)] for key, value in o.state_translations.items()]
+
             return {
                 "vqaresult_n_qubits": o.n_qubits,
                 "vqaresult_instance_nr": o.instance_nr,
@@ -73,16 +83,22 @@ class ResultEncoder(JSONEncoder):
                 "vqaresult_min_energy": o.min_energy,
                 "vqaresult_max_energy": o.max_energy,
                 "vqaresult_measurement_distribution": self.default(o.measurement_distribution),
-                "vqaresult_state_translations": [
-                    [key, self.default(value)] for key, value in o.state_translations.items()
-                ],
+                "vqaresult_state_translations": state_translations,
                 "vqaresult_optimal_makespan": o.optimal_makespan,
                 "vqaresult_max_opt_energy": o.max_opt_energy,
                 "vqaresult_best_expectation_value": o.best_expectation_value,
                 "vqaresult_best_parameter_values": o.best_parameter_values,
+                "vqaresult_min_penalty": o.min_penalty,
+                "vqaresult_min_subopt_energy": o.min_subopt_energy,
             }
 
         if isinstance(o, EVQEBenchmarkResult):
+
+            if o.state_translations is None:
+                state_translations = None
+            else:
+                state_translations = [[key, self.default(value)] for key, value in o.state_translations.items()]
+
             return {
                 "evqeresult_n_qubits": o.n_qubits,
                 "evqeresult_instance_nr": o.instance_nr,
@@ -90,11 +106,11 @@ class ResultEncoder(JSONEncoder):
                 "evqeresult_result": self.default(o.result),
                 "evqeresult_min_energy": o.min_energy,
                 "evqeresult_max_energy": o.max_energy,
-                "evqeresult_state_translations": [
-                    [key, self.default(value)] for key, value in o.state_translations.items()
-                ],
+                "evqeresult_state_translations": state_translations,
                 "evqeresult_optimal_makespan": o.optimal_makespan,
                 "evqeresult_max_opt_energy": o.max_opt_energy,
+                "evqeresult_min_penalty": o.min_penalty,
+                "evqeresult_min_subopt_energy": o.min_subopt_energy,
             }
 
 
@@ -131,6 +147,8 @@ class ResultDecoder(JSONDecoder):
                 "vqaresult_max_opt_energy",
                 "vqaresult_best_expectation_value",
                 "vqaresult_best_parameter_values",
+                "vqaresult_min_penalty",
+                "vqaresult_min_subopt_energy",
             ]
             for key in object_dict.keys()
         ):
@@ -148,6 +166,8 @@ class ResultDecoder(JSONDecoder):
                 "evqeresult_state_translations",
                 "evqeresult_optimal_makespan",
                 "evqeresult_max_opt_energy",
+                "evqeresult_min_penalty",
+                "evqeresult_min_subopt_energy",
             ]
             for key in object_dict.keys()
         ):
@@ -155,32 +175,46 @@ class ResultDecoder(JSONDecoder):
 
     @staticmethod
     def parse_vqaresult(object_dict):
+
+        state_translations = object_dict.get("vqaresult_state_translations")
+        if state_translations is not None:
+            state_translations = dict(state_translations)
+
         return VQABenchmarkResult(
-            n_qubits=object_dict["vqaresult_n_qubits"],
-            instance_nr=object_dict["vqaresult_instance_nr"],
-            seed=object_dict["vqaresult_seed"],
-            expectation_evaluation_counts=object_dict["vqaresult_evaluation_counts"],
-            expectation_values=object_dict["vqaresult_expectation_values"],
-            min_energy=object_dict["vqaresult_min_energy"],
-            max_energy=object_dict["vqaresult_max_energy"],
-            measurement_distribution=object_dict["vqaresult_measurement_distribution"],
-            state_translations=dict(object_dict["vqaresult_state_translations"]),
-            optimal_makespan=object_dict["vqaresult_optimal_makespan"],
+            n_qubits=object_dict.get("vqaresult_n_qubits"),
+            instance_nr=object_dict.get("vqaresult_instance_nr"),
+            seed=object_dict.get("vqaresult_seed"),
+            expectation_evaluation_counts=object_dict.get("vqaresult_evaluation_counts"),
+            expectation_values=object_dict.get("vqaresult_expectation_values"),
+            min_energy=object_dict.get("vqaresult_min_energy"),
+            max_energy=object_dict.get("vqaresult_max_energy"),
+            measurement_distribution=object_dict.get("vqaresult_measurement_distribution"),
+            state_translations=state_translations,
+            optimal_makespan=object_dict.get("vqaresult_optimal_makespan"),
             max_opt_energy=object_dict["vqaresult_max_opt_energy"],
-            best_expectation_value=object_dict["vqaresult_best_expectation_value"],
-            best_parameter_values=object_dict["vqaresult_best_parameter_values"],
+            best_expectation_value=object_dict.get("vqaresult_best_expectation_value"),
+            best_parameter_values=object_dict.get("vqaresult_best_parameter_values"),
+            min_penalty=object_dict.get("vqaresult_min_penalty"),
+            min_subopt_energy=object_dict.get("vqaresult_min_subopt_energy"),
         )
 
     @staticmethod
     def parse_evqeresult(object_dict):
+
+        state_translations = object_dict.get("evqeresult_state_translations")
+        if state_translations is not None:
+            state_translations = dict(state_translations)
+
         return EVQEBenchmarkResult(
-            n_qubits=object_dict["evqeresult_n_qubits"],
-            instance_nr=object_dict["evqeresult_instance_nr"],
-            seed=object_dict["evqeresult_seed"],
-            result=object_dict["evqeresult_result"],
-            min_energy=object_dict["evqeresult_min_energy"],
-            max_energy=object_dict["evqeresult_max_energy"],
-            state_translations=dict(object_dict["evqeresult_state_translations"]),
-            optimal_makespan=object_dict["evqeresult_optimal_makespan"],
-            max_opt_energy=object_dict["evqeresult_max_opt_energy"],
+            n_qubits=object_dict.get("evqeresult_n_qubits"),
+            instance_nr=object_dict.get("evqeresult_instance_nr"),
+            seed=object_dict.get("evqeresult_seed"),
+            result=object_dict.get("evqeresult_result"),
+            min_energy=object_dict.get("evqeresult_min_energy"),
+            max_energy=object_dict.get("evqeresult_max_energy"),
+            state_translations=state_translations,
+            optimal_makespan=object_dict.get("evqeresult_optimal_makespan"),
+            max_opt_energy=object_dict.get("evqeresult_max_opt_energy"),
+            min_penalty=object_dict.get("evqeresult_min_penalty"),
+            min_subopt_energy=object_dict.get("evqeresult_min_subopt_energy"),
         )
